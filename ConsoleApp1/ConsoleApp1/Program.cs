9da63ac3f97909383ed94f093efca62f5d3710df
Meghan <meghantomback@gmail.com>
2025-11-09 19:22:21 -0500

    bubble sort implemented

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void StudentBubbleSort(int[] anArray)
+        {
+            for (int i = 0; i < anArray.Length; i++)
+            {
+                bool sorting = false;
+                for (int j = 0; j < anArray.Length - 1; j++)
+                {
+                    if (anArray[j] > anArray[j + 1])
+                    {
+                        sorting = true;
+                        (anArray[j], anArray[j + 1]) = (anArray[j + 1], anArray[j]);
+                    }
+                }
+                if (!sorting) { break; }
+            }
+        static void Main(string[] args)
+        {
+            int[] array1 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            int[] array2 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+        }
+    }
+}
