20fb9c41658e07f4ff1cc3e04806589e6a72696e
Meghan <meghantomback@gmail.com>
2025-11-09 19:32:04 -0500

    main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,10 +17,34 @@
                 }
                 if (!sorting) { break; }
             }
+        }
         static void Main(string[] args)
         {
             int[] array1 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
             int[] array2 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+            Console.WriteLine("Array1 before any sorting: ");
+            foreach(int num in array1)
+            {
+                Console.WriteLine(num);
+            }
+            StudentBubbleSort(array1);
+            Console.WriteLine("Array1 after bubble sort: ");
+             foreach (int num in array1)
+             {
+                 Console.WriteLine(num);
+             }
+             Console.WriteLine("Array2 before any sorting: ");
+            foreach (int num in array2)
+            {
+                Console.WriteLine(num);
+            }
+            Array.Sort(array2);
+            Console.WriteLine("Array2 after built in sort: ");
+            foreach (int num in array1)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
